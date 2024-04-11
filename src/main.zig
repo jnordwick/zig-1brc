3258b9b41798b2505e4a0ef16c89aa6b0e6ac385
Jason Nordwick <jnordwick@proton.me>
2024-04-11 01:13:01 -0400

    initial

---
+++ src/main.zig
@@ -0,0 +1,236 @@
+const std = @import("std");
+const lin = std.os.linux;
+const hm = std.hash_map;
+const Allocator = std.mem.Allocator;
+
+pub const cache_line_size = 64;
+pub const nthreads = 12;
+pub const ht_capacity = 10000;
+pub const buf_writer_size = 1024 * 500;
+
+pub fn map_file(fname: []const u8) ![]u8 {
+    const file = try std.fs.cwd().openFile(fname, .{});
+    const stat = try file.stat();
+    const len = stat.size;
+    const int_addr = lin.mmap(null, stat.size, lin.PROT.READ, .{ .TYPE = .PRIVATE }, file.handle, 0);
+    const addr: [*]u8 = @ptrFromInt(int_addr);
+    return addr[0..len];
+}
+
+pub const Map = hm.HashMapUnmanaged([]const u8, Stat, Context, 60);
+
+pub const PerThread = struct {
+    const data_size = @sizeOf(Map) + 4 * ht_capacity;
+    const _pad_size = b: {
+        const t = cache_line_size - (data_size % cache_line_size);
+        break :b if (t == 0) 0 else t;
+    };
+
+    t: std.Thread = undefined,
+    m: Map = Map{},
+    _pad: [_pad_size]u8 = [_]u8{0} ** _pad_size,
+
+    pub fn init(this: @This()) void {
+        for (0..ht_capacity) |n| {
+            this.i[n] = n;
+        }
+    }
+};
+
+pub const Stat = struct {
+    sum: i32 = 0,
+    n: u32 = 0,
+};
+
+const Context = struct {
+    pub fn hash(self: @This(), s: []const u8) u64 {
+        _ = self;
+        return std.hash.XxHash64.hash(0, s);
+    }
+    pub fn eql(self: @This(), a: []const u8, b: []const u8) bool {
+        _ = self;
+        return std.mem.eql(u8, a, b);
+    }
+};
+
+pub fn make_buffered_writer(under: anytype) std.io.BufferedWriter(buf_writer_size, @TypeOf(under)) {
+    return .{ .unbuffered_writer = under };
+}
+
+pub fn parse_num(str: []const u8) i32 {
+    var sign: i32 = 10;
+    var p: usize = 0;
+    var num: i32 = 0;
+    if (str[0] == '-') {
+        sign = -10;
+        p = 1;
+    }
+    while (p < str.len) : (p += 1) {
+        if (str[p] == '.')
+            continue;
+        const dig = str[p] - '0';
+        num = num * 10 + dig;
+    }
+    const n = num * sign;
+    return n;
+}
+
+pub fn parse_line(span: []const u8, pos: u64) struct { []const u8, i32, u64 } {
+    var p = pos;
+    while (span[p] != ';') : (p += 1) {}
+    const name = span[pos..p];
+    p += 1;
+    const dec_start = p;
+    while (span[p] != '\n') : (p += 1) {}
+    const dec = span[dec_start..p];
+    p += 1;
+    const num = parse_num(dec);
+    return .{ name, num, p };
+}
+
+pub fn parse_span(span: []const u8, pt: *PerThread) void {
+    var p: u64 = 0;
+    while (p < span.len) {
+        const x = parse_line(span, p);
+        var r = pt.m.getOrPutAssumeCapacity(x[0]);
+        if (!r.found_existing) {
+            r.value_ptr.sum = x[1];
+            r.value_ptr.n = 1;
+        } else {
+            r.value_ptr.sum += x[1];
+            r.value_ptr.n += 1;
+        }
+        p = x[2];
+    }
+}
+
+pub fn next_span(span: []const u8, offset: u64, len: u64) []const u8 {
+    const end = offset + len;
+    if (end >= span.len)
+        return span[offset..span.len];
+    var pos = end;
+    while (span[pos] != '\n') : (pos += 1) {}
+    pos += 1;
+    return span[offset..pos];
+}
+
+pub fn spin_thread(pt: *PerThread, span: []const u8, alloc: Allocator) !void {
+    try pt.m.ensureTotalCapacity(alloc, ht_capacity);
+    pt.t = try std.Thread.spawn(.{}, parse_span, .{ span, pt });
+}
+
+// BUG: you can have such a small thread and too many threads where
+// the same line gets indexed by two threads if they are given the same
+// line to start on. the fix is to have spin_out find the new line after
+// the offset calculation and only pass in offsets that start the next
+// line. right now each threads find their own new line prior to the offset.
+pub fn spin_out(comptime N: comptime_int, span: []const u8, alloc: Allocator) !Map {
+    var maps = [_]PerThread{.{}} ** (N + 1);
+    const size: u64 = 1 + span.len / N;
+    var offset: u64 = 0;
+    for (0..N) |n| {
+        const nspan = next_span(span, offset, size);
+        offset += nspan.len;
+        try spin_thread(&maps[n], nspan, alloc);
+    }
+    std.debug.assert(offset == span.len);
+
+    maps[0].t.join();
+    var tlmap = &maps[0].m;
+
+    for (1..N) |n| {
+        maps[n].t.join();
+        var it = maps[n].m.iterator();
+        while (it.next()) |e| {
+            var r = tlmap.getOrPutAssumeCapacity(e.key_ptr.*);
+            if (!r.found_existing) {
+                r.value_ptr.* = e.value_ptr.*;
+            } else {
+                r.value_ptr.sum += e.value_ptr.sum;
+                r.value_ptr.n += e.value_ptr.n;
+            }
+        }
+    }
+
+    return maps[0].m;
+}
+
+pub fn slice_compare(ctx: u32, l: []const u8, r: []const u8) bool {
+    _ = ctx;
+    return std.mem.lessThan(u8, l, r);
+}
+
+// This function is ugly af, I know. Its 23:15, and I'm tired.
+pub fn print_num(cnum: i32, buf: *[32]u8) ![]u8 {
+    const is_neg = cnum < 0;
+    const num: u32 = @intCast(if (is_neg) -cnum else cnum);
+    var n: u32 = (num / 10) + @intFromBool(num % 10 >= 5);
+    if (n == 0) {
+        buf[29] = '0';
+        buf[30] = '.';
+        buf[31] = '0';
+        return buf[29..];
+    }
+    var pos: u32 = buf.len - 1;
+    buf[pos] = '0' + @as(u8, @intCast(n % 10));
+    n /= 10;
+    buf[pos - 1] = '.';
+    if (n == 0) {
+        buf[pos - 2] = '0';
+        return buf[pos - 2 ..];
+    }
+    pos -= 2;
+    while (n > 0) {
+        buf[pos] = '0' + @as(u8, @intCast(n % 10));
+        n /= 10;
+        pos -= 1;
+    }
+    if (is_neg) {
+        buf[pos] = '-';
+    } else {
+        pos += 1;
+    }
+    return buf[pos..];
+}
+
+pub fn print_stats(m: *const Map, ks: [][]const u8) !void {
+    const stdout = std.io.getStdOut();
+    var bout = make_buffered_writer(stdout.writer());
+    var num_buf: [32]u8 = undefined;
+
+    for (ks) |k| {
+        const stat = m.get(k).?;
+        const mean = @divTrunc(stat.sum, @as(i32, @intCast(stat.n)));
+        const num = try print_num(mean, &num_buf);
+        _ = try bout.write(k);
+        _ = try bout.write(";");
+        _ = try bout.write(num);
+        _ = try bout.write("\n");
+    }
+    try bout.flush();
+}
+
+pub fn sort_map(m: *const Map, ks: *[ht_capacity][]const u8) u32 {
+    var it = m.keyIterator();
+    const sz = m.size;
+    var i: u64 = 0;
+    while (it.next()) |e| {
+        ks[i] = e.*;
+        i += 1;
+    }
+    const sl = ks[0..sz];
+    std.sort.block([]const u8, sl, @as(u32, 0), slice_compare);
+    return sz;
+}
+
+pub fn main() !void {
+    const ffname = "data/zig-1brc/measurements.txt";
+    var ks: [ht_capacity][]u8 = undefined;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const alloc = arena.allocator();
+
+    const span = try map_file(ffname[0..]);
+    const map = try spin_out(nthreads, span, alloc);
+    const sz = sort_map(&map, &ks);
+    try print_stats(&map, ks[0..sz]);
+}
---
+++ src/tests.zig
@@ -0,0 +1,105 @@
+const std = @import("std");
+const tt = std.testing;
+const b = @import("main.zig");
+const pp = std.debug.print;
+
+test "mmap" {
+    const ffname = "data/measurements.txt";
+    const span = try b.map_file(ffname[0..]);
+    pp("mapped {s} for {d} bytes\n", .{ ffname, span.len });
+}
+
+test "parse num" {
+    try tt.expectEqual(@as(i32, 1230), b.parse_num(@as([]const u8, "12.3"[0..])));
+    try tt.expectEqual(@as(i32, -1230), b.parse_num(@as([]const u8, "-12.3"[0..])));
+    try tt.expectEqual(@as(i32, 200), b.parse_num(@as([]const u8, "2.0"[0..])));
+    try tt.expectEqual(@as(i32, -230), b.parse_num(@as([]const u8, "-2.3"[0..])));
+}
+
+test "parseline" {
+    const text: []const u8 = "one;3.4\ntwo;-12.3\nthree;0.1\n";
+
+    const a = b.parse_line(text, 0);
+    try tt.expectEqualSlices(u8, @as([]const u8, "one"[0..]), a[0]);
+    try tt.expectEqual(@as(i32, 340), a[1]);
+    try tt.expectEqual(@as(u64, 8), a[2]);
+
+    const bb = b.parse_line(text, a[2]);
+    try tt.expectEqualSlices(u8, @as([]const u8, "two"[0..]), bb[0]);
+    try tt.expectEqual(@as(i32, -1230), bb[1]);
+    try tt.expectEqual(@as(u64, 18), bb[2]);
+
+    const c = b.parse_line(text, bb[2]);
+    try tt.expectEqualSlices(u8, @as([]const u8, "three"[0..]), c[0]);
+    try tt.expectEqual(@as(i32, 10), c[1]);
+    try tt.expectEqual(@as(u64, text.len), c[2]);
+}
+
+test "parse range" {
+    const text: []const u8 = "zero;9.9\none;3.4\ntwo;-12.3\none;-0.1\n";
+    var m = b.PerThread{};
+    try m.m.ensureTotalCapacity(tt.allocator, b.ht_capacity);
+    defer m.m.deinit(tt.allocator);
+    b.parse_span(text, &m);
+    const k1: []const u8 = "one";
+    const k2: []const u8 = "two";
+    const v1 = m.m.get(k1);
+    const v2 = m.m.get(k2);
+    try tt.expectEqual(b.Stat{ .sum = 330, .n = 2 }, v1);
+    try tt.expectEqual(b.Stat{ .sum = -1230, .n = 1 }, v2);
+}
+
+test "spin_out" {
+    const text: []const u8 = "one;1.2\nzero;9.9\none;3.4\ntwo;-12.3\none;-0.1\ntwo;-1.2\n";
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const alloc = arena.allocator();
+    const m = try b.spin_out(2, text, alloc);
+
+    const k0: []const u8 = "zero";
+    const k1: []const u8 = "one";
+    const k2: []const u8 = "two";
+    const v0 = m.get(k0).?;
+    const v1 = m.get(k1).?;
+    const v2 = m.get(k2).?;
+    try tt.expectEqual(b.Stat{ .sum = 990, .n = 1 }, v0);
+    try tt.expectEqual(b.Stat{ .sum = 450, .n = 3 }, v1);
+    try tt.expectEqual(b.Stat{ .sum = -1350, .n = 2 }, v2);
+}
+
+test "map and spin" {
+    const file = "data/mill.txt";
+    const text = try b.map_file(file[0..]);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const alloc = arena.allocator();
+    const m = try b.spin_out(16, text, alloc);
+
+    var sum: u32 = 0;
+    var it = m.iterator();
+    while (it.next()) |e| {
+        sum += e.value_ptr.n;
+    }
+    try tt.expectEqual(@as(u32, 1000000), sum);
+}
+
+test "sort map" {
+    const text: []const u8 = "zero;9.9\none;3.4\ntwo;-12.3\none;-0.1\n";
+    var ks: [b.ht_capacity][]u8 = undefined;
+    var m = b.PerThread{};
+    try m.m.ensureTotalCapacity(tt.allocator, b.ht_capacity);
+    defer m.m.deinit(tt.allocator);
+    b.parse_span(text, &m);
+    const sz = b.sort_map(&m.m, &ks);
+    const rs: [3][]const u8 = .{ "one", "two", "zero" };
+    try tt.expectEqual(3, sz);
+    for (0..rs.len) |i| {
+        try tt.expectEqualSlices(u8, rs[i], ks[i]);
+    }
+}
+
+test "print num" {
+    var buf: [32]u8 = undefined;
+    try tt.expectEqualSlices(u8, "12.3", try b.print_num(1234, &buf));
+    try tt.expectEqualSlices(u8, "0.0", try b.print_num(3, &buf));
+    try tt.expectEqualSlices(u8, "0.1", try b.print_num(5, &buf));
+    try tt.expectEqualSlices(u8, "-8.3", try b.print_num(-825, &buf));
+}
